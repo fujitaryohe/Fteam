830c4a64c836cbd2b63282388b23419ece86587d
fujita <ase19410092@c-league.jp>
2021-11-18 13:45:37 +0900

    gプロジェクト ファイルを追加します。

---
+++ CampApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CampApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
